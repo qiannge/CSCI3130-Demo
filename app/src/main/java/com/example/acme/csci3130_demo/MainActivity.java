1093265decf8d8a691fa2a47ed60357b7b4948c8
qiannge <1045774426@qq.com>
2018-05-19 21:40:32 -0300

    Yingzhen B00692487

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void enter(View view){
+        EditText edit = (EditText) findViewById(R.id.editText);
+        String message = edit.getText().toString();
+        TextView text = findViewById(R.id.textView);
+        text.setText(message);
     }
 }
